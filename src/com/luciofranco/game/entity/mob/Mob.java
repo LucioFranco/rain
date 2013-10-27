d0da359ec5363113360c09b01c22feee12274588
luciofranco <luciofranco@rocketmail.com>
2013-10-27 18:17:43 -0400

    Added mob, player, and .gitignore

--- src/com/luciofranco/game/entity/mob/Mob.java
+++ src/com/luciofranco/game/entity/mob/Mob.java
@@ -6,6 +6,16 @@ import com.luciofranco.game.graphics.Sprite;
 public abstract class Mob extends Entity{
 	
 	protected Sprite sprite;
+	protected int dir = 0;
+	protected boolean moving = false;
 	
+	public void move() {
+	}
 	
+	public void update() {
+	}
+	
+	private boolean collision() {
+		return false;
+	}
 }
---
+++ src/com/luciofranco/game/entity/mob/Player.java
@@ -0,0 +1,23 @@
+package com.luciofranco.game.entity.mob;
+
+public class Player extends Mob{
+	
+	public Player() {
+		
+	}
+	
+	public Player(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void update() {
+		
+	}
+	
+	public void render() {
+		
+	}
+	
+	
+}
